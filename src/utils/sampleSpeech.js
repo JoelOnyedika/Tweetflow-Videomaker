270eea0b84b2ca4ebe82b276846883a301f655d7
JoelOnyedika <86019717+JoelOnyedika@users.noreply.github.com>
2024-12-21 22:24:08 +0000

    Now added ssample speech from replit

--- src/utils/sampleSpeech.js
+++ src/utils/sampleSpeech.js
@@ -1,14 +1,32 @@
-import {data} from './constants'
+import { data } from "./constants";
+import fs from "fs";
+import textToSpeech from "@google-cloud/text-to-speech";
 
-export default function speak() {
-    let text = ""
-    const loop = data.captions.forEach(element => {
-        text += element.text + " "
-    });
-    const utterance = new SpeechSynthesisUtterance(text)
+export default async function speak() {
+  const client = new textToSpeech.TextToSpeechClient();
 
-    const voices = speechSynthesis.getVoices()
-    utterance.voice = voices[0]
+  // Combine all text from captions
+  let text = "";
+  data.captions.forEach((element) => {
+    text += element.text + " ";
+  });
 
-    speechSynthesis.speak(utterance)
+  // Configure the request
+  const request = {
+    input: { text },
+    voice: { languageCode: "en-US", ssmlGender: "NEUTRAL" },
+    audioConfig: { audioEncoding: "MP3" },
+  };
+
+  try {
+    // Generate speech
+    const [response] = await client.synthesizeSpeech(request);
+
+    // Save the audio to the current directory
+    const fileName = "output.mp3";
+    fs.writeFileSync(fileName, response.audioContent, "binary");
+    console.log(`Audio content written to file: ${fileName}`);
+  } catch (error) {
+    console.error("Error generating speech:", error);
+  }
 }
